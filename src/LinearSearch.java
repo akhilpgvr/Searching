88192b3bf5dba4559aa3db56a1db4a02e7765900
Akhil <akhilpgvr@gmail.com>
2024-08-03 21:30:57 +0530

    adding linear search

---
+++ src/LinearSearch.java
@@ -0,0 +1,13 @@
+public class LinearSearch {
+
+    public void performLinearSearch(int[] arr,int size, int key) throws Exception {
+
+        for(int i=0; i<size; i++) {
+            if(arr[i] == key) {
+                System.out.println("Element found at Position: "+ (i+1));
+                return;
+            }
+        }
+        throw new Exception("Element not found in the list");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+
+        int[] arr = {3,9,1,8,6,7,2,13,11};
+        int size = arr.length;
+        System.out.println("...............................");
+        LinearSearch liSearch = new LinearSearch();
+        liSearch.performLinearSearch(arr, size, 13);
     }
 }
